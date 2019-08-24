3ea40e8d1111381efd662bf5152683ad8faa91e1
Herman Starikov <hermanstarikov@gmail.com>
2019-08-23 23:19:40 -0400

    update readme

---
+++ README.md
@@ -0,0 +1,28 @@
+Responsive React App
+--------------------
+
+<!-- <a href="https://npmjs.com/package/responsive-react-app" target="\_parent">
+  <img alt="" src="https://img.shields.io/npm/dm/responsive-react-app.svg" />
+</a> -->
+<a href="https://github.com/Hermanya/responsive-react-app" target="\_parent">
+  <img alt="" src="https://img.shields.io/github/stars/Hermanya/responsive-react-app.svg?style=social&label=Star" />
+</a>
+<a href="https://twitter.com/hermanhasawish" target="\_parent">
+  <img alt="" src="https://img.shields.io/twitter/follow/hermanhasawish.svg?style=social&label=Follow" />
+</a>
+
+![Demo Recording](./recording.gif)
+
+[👉 See the Website](https://hermanya.github.io/responsive-react-app)
+
+[👉 Basic demo on CodeSendbox](https://codesandbox.io/s/bold-noether-fybj1)
+
+## Features
+
+- Lightweight
+- Easy to use
+- RESPONSIVE
+
+## License
+
+[MIT](./LICENSE)
--- example/src/components/usage.tsx
+++ example/src/components/usage.tsx
@@ -17,9 +17,15 @@ const Tab = styled(Link)\`
   text-decoration: none;
   font-size: 0.7rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 60px;
 \`
+
+const Icon = styled.div\`
+  font-size: 24px;
+\`;
 
 const App = () =>
   <ResponsiveReactApp
@@ -27,16 +33,19 @@ const App = () =>
       <Tab to="/">Page 1</Tab>
       <Tab to="/2">Page 2</Tab>
       <Tab to="/3">Page 3</Tab>
-      <Tab to="/4">Page 4</Tab>
+      <Tab to="/4">
+        <Icon>🤟</Icon>
+        Page 4
+      </Tab>
     </>}
     paths={[
       '/', '/2', '/3', '/4'
     ]}
   >
-    <div>Page 1</div>
-    <div>Page 2</div>
-    <div>Maybe Page 3</div>
-    <div>And Maybe Page 4</div>
+    <Page>Page 1</Page>
+    <Page>Page 2</Page>
+    <Page>Maybe Page 3</Page>
+    <Page>And Maybe Page 4</Page>
   </ResponsiveReactApp>
 `;
 
@@ -44,6 +53,13 @@ function Usage() {
   return (
     <div css="padding: 32px;">
       <h2>Usage</h2>
+      <p>
+        I put together an example on{" "}
+        <a href="https://codesandbox.io/s/basic-react-responsive-app-fybj1">
+          CodeSendbox
+        </a>
+        .
+      </p>
       <p>Install the package:</p>
       <code>
         <strong>npm install responsive-react-app</strong> react react-router-dom
